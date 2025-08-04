e5f9fc69a0a6c70de04a3dce795dd88495afdb52
Unyielding-Zookeeper <freddiechavira@gmail.com>
2025-08-03 17:32:08 -0700

    fix: return value from getComputerChoice and add playGame

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const getComputerChoice = () => {
     const randomNumber = Math.floor(Math.random() * 3);
-}
+    if (randomNumber === 0) {
+        return "rock";
+    } else if (randomNumber === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+    }
+    
 function getHumanChoice() {
     let choice = prompt('Choose rock, paper, or scissors');
     if (choice !== null) {
@@ -8,14 +16,25 @@ function getHumanChoice() {
     }
     return choice;
 }
-let humanScore = 0;
-let computerScore = 0;
 
-function playRound(humanChoice, computerChoice) {
-    console.log("Human picked:", humanChoice);
-    console.log("Computer picked:", computerChoice);
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+    function playRound(humanChoice, computerChoice) {
+        console.log("Human picked:", humanChoice);
+        console.log("Computer picked:", computerChoice);
+    }
+playRound(getHumanChoice(), getComputerChoice());
+playRound(getHumanChoice(), getComputerChoice());
+playRound(getHumanChoice(), getComputerChoice());
+playRound(getHumanChoice(), getComputerChoice());
+playRound(getHumanChoice(), getComputerChoice());
+playRound(human, computer);
+
 }
+
 const human = getHumanChoice();
 const computer = getComputerChoice();
 
-playRound(human, computer);
+
+playGame();
