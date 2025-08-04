9f9a54cc2fcb71911b1ef92c7bb9a08df8686aec
Unyielding-Zookeeper <freddiechavira@gmail.com>
2025-08-03 17:27:58 -0700

    Finished

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 const getComputerChoice = () => {
     const randomNumber = Math.floor(Math.random() * 3);
 }
+function getHumanChoice() {
+    let choice = prompt('Choose rock, paper, or scissors');
+    if (choice !== null) {
+        choice = choice.toLowerCase();
+    }
+    return choice;
+}
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    console.log("Human picked:", humanChoice);
+    console.log("Computer picked:", computerChoice);
+}
+const human = getHumanChoice();
+const computer = getComputerChoice();
+
+playRound(human, computer);
